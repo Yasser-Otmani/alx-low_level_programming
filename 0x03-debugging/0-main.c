55bee05115b7e13daa8bdeaf6405dca5b2b26a09
Yasser-Otmani <otmaniyasser7@gmail.com>
2023-06-22 19:55:57 -0700

    debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - Fonction principale
- * Return: 0
- */
+* main - Test function for positive or negative
+* Return: 0
+*/
 int main(void)
 {
-int i = 98;
-
+int i;
+i = 0;
 positive_or_negative(i);
-
 return (0);
 }
