30b7cd0f66b8ea5af3e16531c0037b8be1e85546
Yasser-Otmani <otmaniyasser7@gmail.com>
2023-06-23 08:01:27 -0700

    0-isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * _isupper - checks for uppercase character 
- * @c: Variable text
- * Return:0.
+ * _isupper - checks for uppercase character
+ * @c: Variable texte
+ * Return: 1 si le caractère est une majuscule, 0 sinon.
  */
 int _isupper(int c)
 {
-	if (c >= 'A'&& c <='Z')
-	{
-		return (1);
-	}
-	else 
-	{
-		return (0);
-	}
+    if (c >= 'A' && c <= 'Z')
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
 }
